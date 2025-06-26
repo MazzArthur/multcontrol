f2485293d7d636cf5e8a2f2d903a724bd1e4182a
MazzArthur <58313259+MazzArthur@users.noreply.github.com>
2025-06-26 08:36:56 -0300

    Update captcha_script_content.js

--- userscripts_content/captcha_script_content.js
+++ userscripts_content/captcha_script_content.js
@@ -8,6 +8,7 @@
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // @grant        GM_getValue
+// @grant        GM_setValue
 // @grant        GM_notification
 // @grant        unsafeWindow
 // @grant        GM_xmlhttpRequest
